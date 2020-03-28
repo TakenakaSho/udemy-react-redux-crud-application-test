cdd93b256bfa34faad3c449b6c19dbd89af84ca5
武中翔 <takenakashou@takenakashounoMacBook-Pro.local>
2020-03-28 14:25:45 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("click");
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log("click");
-        }}
-      />
+      <Neko />
+      <Neko />
+      <Neko />
     </div>
   );
-}
+};
+
+const Neko = () => {
+  return <div>nyan</div>;
+};
 
 export default App;
